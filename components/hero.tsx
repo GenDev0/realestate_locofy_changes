4d29b682c238fedfae637962a4b1b8093708d328
GenDev0 <dev.gen.info.0@gmail.com>
2023-07-25 19:07:25 +0100

    make changes

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,92 +1,108 @@
 import type { NextPage } from "next";
 import "antd/dist/antd.min.css";
-import { Menu, Dropdown, Button } from "antd";
-import {
-  DownOutlined,
-  ArrowLeftOutlined,
-  ArrowRightOutlined,
-  CalendarOutlined,
-  CheckOutlined,
-  ClockCircleOutlined,
-  CloseOutlined,
-  DeleteOutlined,
-  EditOutlined,
-  ExclamationCircleOutlined,
-  HeartOutlined,
-  LeftOutlined,
-  LockOutlined,
-  MailOutlined,
-  PaperClipOutlined,
-  PhoneOutlined,
-  QuestionCircleOutlined,
-  ReloadOutlined,
-  RightOutlined,
-  SearchOutlined,
-  SendOutlined,
-  ShareAltOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
+import { Menu, Dropdown } from "antd";
+import { DownOutlined } from "@ant-design/icons";
 import { useRouter } from "next/router";
+
+const defaultCities = [
+  {
+    key: "1",
+    label: <a onClick={(e) => e.preventDefault()}>Los Angeles</a>,
+  },
+  {
+    key: "2",
+    label: <a onClick={(e) => e.preventDefault()}>Chicago</a>,
+  },
+  {
+    key: "3",
+    label: <a onClick={(e) => e.preventDefault()}>Berlin</a>,
+  },
+  {
+    key: "4",
+    label: <a onClick={(e) => e.preventDefault()}>New York</a>,
+  },
+];
+
+const defaultTypes = [
+  {
+    key: "1",
+    label: <a onClick={(e) => e.preventDefault()}>One-bedroom apartments</a>,
+  },
+  {
+    key: "2",
+    label: <a onClick={(e) => e.preventDefault()}>Two-bedroom apartments</a>,
+  },
+  {
+    key: "3",
+    label: <a onClick={(e) => e.preventDefault()}>Three-bedroom apartments</a>,
+  },
+  {
+    key: "4",
+    label: (
+      <a onClick={(e) => e.preventDefault()}>
+        Four or more bedroom apartments/houses
+      </a>
+    ),
+  },
+];
+
+const defaultRentRanges = [
+  {
+    key: "1",
+    label: <a onClick={(e) => e.preventDefault()}>$500-$2000</a>,
+  },
+  {
+    key: "2",
+    label: <a onClick={(e) => e.preventDefault()}>$2500-$10000</a>,
+  },
+  {
+    key: "3",
+    label: <a onClick={(e) => e.preventDefault()}>$10000+</a>,
+  },
+];
 
 const Hero: NextPage = () => {
   const router = useRouter();
 
   const onSearchCTAClick = () => {
-    router.push("/");
+    router.push("/properties");
   };
 
   return (
-    <div className="self-stretch flex flex-col py-[120px] px-[30px] items-center justify-start bg-[url(/hero-section@3x.png)] bg-cover bg-no-repeat bg-[top] text-center text-33xl text-gray-white font-body-regular-400">
-      <div className="self-stretch flex flex-col items-center justify-center gap-[62px] max-w-[95%px]">
-        <div className="self-stretch flex flex-col items-center justify-start gap-[24px] md:max-w-full">
-          <div className="self-stretch relative leading-[72px] font-semibold">
+    <div className='self-stretch flex flex-col py-[120px] px-[30px] items-center justify-start bg-[url(/hero-section@3x.png)] bg-cover bg-no-repeat bg-[top] text-center text-33xl text-gray-white font-body-regular-400'>
+      <div className='self-stretch flex flex-col items-center justify-center gap-[62px] max-w-[95%px]'>
+        <div className='self-stretch flex flex-col items-center justify-start gap-[24px] md:max-w-full'>
+          <div className='self-stretch relative leading-[72px] font-semibold'>
             Find Your Dream Home
           </div>
-          <div className="self-stretch relative text-xl leading-[28px] font-body-regular-600 text-primary-50">
+          <div className='self-stretch relative text-xl leading-[28px] font-body-regular-600 text-primary-50'>
             Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
             posuere cubilia curae; Proin sodales ultrices nulla blandit
             volutpat.
           </div>
         </div>
-        <div className="self-stretch flex flex-col items-center justify-start gap-[17px] text-left text-base text-primary-700 font-body-regular-600">
-          <div className="flex flex-row items-start justify-start gap-[10px]">
-            <button className="cursor-pointer [border:none] py-3 px-6 bg-primary-500 rounded overflow-hidden flex flex-row items-start justify-start">
-              <div className="relative text-base leading-[24px] font-medium font-body-regular-600 text-gray-white text-center">
+        <div className='self-stretch flex flex-col items-center justify-start gap-[17px] text-left text-base text-primary-700 font-body-regular-600'>
+          <div className='flex flex-row items-start justify-start gap-[10px]'>
+            <button className='cursor-pointer [border:none] py-3 px-6 bg-primary-500 rounded overflow-hidden flex flex-row items-start justify-start'>
+              <div className='relative text-base leading-[24px] font-medium font-body-regular-600 text-gray-white text-center'>
                 Rent
               </div>
             </button>
-            <button className="cursor-pointer [border:none] py-3 px-6 bg-gray-white rounded overflow-hidden flex flex-row items-start justify-start">
-              <div className="relative text-base leading-[24px] font-medium font-body-regular-600 text-primary-400 text-center">
+            <button className='cursor-pointer [border:none] py-3 px-6 bg-gray-white rounded overflow-hidden flex flex-row items-start justify-start'>
+              <div className='relative text-base leading-[24px] font-medium font-body-regular-600 text-primary-400 text-center'>
                 Sale
               </div>
             </button>
           </div>
-          <div className="self-stretch flex flex-row flex-wrap items-start justify-center">
-            <div className="flex-1 rounded-lg bg-gray-white flex flex-row py-8 px-[62px] box-border items-center justify-between max-w-[1400px] md:w-[300px] md:flex-col md:gap-[20px] md:items-start md:justify-start md:ml-[auto] md:mr-[auto]">
-              <div className="w-[137px] flex flex-col items-start justify-start gap-[16px] text-center">
-                <div className="relative leading-[24px] capitalize font-semibold">
+          <div className='self-stretch flex flex-row flex-wrap items-start justify-center'>
+            <div className='flex-1 rounded-lg bg-gray-white flex flex-row py-8 px-[62px] box-border items-center justify-between max-w-[1400px] md:w-[300px] md:flex-col md:gap-[20px] md:items-start md:justify-start md:ml-[auto] md:mr-[auto]'>
+              <div className='w-[137px] flex flex-col items-start justify-start gap-[16px] text-center'>
+                <div className='relative leading-[24px] capitalize font-semibold'>
                   Locations
                 </div>
                 <Dropdown
-                  overlay={
-                    <Menu>
-                      {(
-                        [
-                          { value: "New York" },
-                          { value: "Los Angeles" },
-                          { value: "Chicago" },
-                          { value: "Berlin" },
-                        ] as any
-                      ).map((option: any, index: number) => (
-                        <Menu.Item key={index}>
-                          <a onClick={(e) => e.preventDefault()}>
-                            {option.value || ""}
-                          </a>
-                        </Menu.Item>
-                      ))}
-                    </Menu>
-                  }
-                  placement="bottomLeft"
+                  menu={{ items: defaultCities }}
+                  placement='bottomLeft'
                   trigger={["hover"]}
                 >
                   <a onClick={(e) => e.preventDefault()}>
@@ -95,32 +111,14 @@ const Hero: NextPage = () => {
                   </a>
                 </Dropdown>
               </div>
-              <div className="w-[177px] flex flex-col items-start justify-start gap-[16px]">
-                <div className="relative leading-[24px] capitalize font-semibold flex items-end w-[150px]">
+              <div className='w-[177px] flex flex-col items-start justify-start gap-[16px]'>
+                <div className='relative leading-[24px] capitalize font-semibold flex items-end w-[150px]'>
                   Property Type
                 </div>
                 <Dropdown
-                  className="self-stretch"
-                  overlay={
-                    <Menu>
-                      {(
-                        [
-                          { value: "Studio apartments" },
-                          { value: "One-bedroom apartments" },
-                          { value: "Two-bedroom apartments" },
-                          { value: "Three-bedroom apartments" },
-                          { value: "Four or more bedroom apartments/houses" },
-                        ] as any
-                      ).map((option: any, index: number) => (
-                        <Menu.Item key={index}>
-                          <a onClick={(e) => e.preventDefault()}>
-                            {option.value || ""}
-                          </a>
-                        </Menu.Item>
-                      ))}
-                    </Menu>
-                  }
-                  placement="bottomLeft"
+                  className='self-stretch'
+                  menu={{ items: defaultTypes }}
+                  placement='bottomLeft'
                   trigger={["hover"]}
                 >
                   <a onClick={(e) => e.preventDefault()}>
@@ -129,30 +127,14 @@ const Hero: NextPage = () => {
                   </a>
                 </Dropdown>
               </div>
-              <div className="w-[155px] flex flex-col items-start justify-start gap-[16px]">
-                <div className="relative leading-[24px] capitalize font-semibold flex items-end w-[150px]">
+              <div className='w-[155px] flex flex-col items-start justify-start gap-[16px]'>
+                <div className='relative leading-[24px] capitalize font-semibold flex items-end w-[150px]'>
                   Rent Range
                 </div>
                 <Dropdown
-                  className="self-stretch"
-                  overlay={
-                    <Menu>
-                      {(
-                        [
-                          { value: "$500-$2000" },
-                          { value: "$2500-$10000" },
-                          { value: "$10000+" },
-                        ] as any
-                      ).map((option: any, index: number) => (
-                        <Menu.Item key={index}>
-                          <a onClick={(e) => e.preventDefault()}>
-                            {option.value || ""}
-                          </a>
-                        </Menu.Item>
-                      ))}
-                    </Menu>
-                  }
-                  placement="bottomLeft"
+                  className='self-stretch'
+                  menu={{ items: defaultRentRanges }}
+                  placement='bottomLeft'
                   trigger={["hover"]}
                 >
                   <a onClick={(e) => e.preventDefault()}>
@@ -162,12 +144,12 @@ const Hero: NextPage = () => {
                 </Dropdown>
               </div>
               <button
-                className="cursor-pointer [border:none] py-3 px-6 bg-primary-500 rounded w-[102px] flex flex-row box-border items-center justify-center hover:bg-steelblue"
+                className='cursor-pointer [border:none] py-3 px-6 bg-primary-500 rounded w-[102px] flex flex-row box-border items-center justify-center hover:bg-steelblue'
                 onClick={onSearchCTAClick}
               >
-                <button className="cursor-pointer [border:none] p-0 bg-[transparent] relative text-base leading-[24px] font-medium font-body-regular-600 text-gray-white text-center inline-block">
+                <div className='cursor-pointer [border:none] p-0 bg-[transparent] relative text-base leading-[24px] font-medium font-body-regular-600 text-gray-white text-center inline-block'>
                   Search
-                </button>
+                </div>
               </button>
             </div>
           </div>
--- components/property-card.tsx
+++ components/property-card.tsx
@@ -1,126 +1,105 @@
 import type { NextPage } from "next";
-import { useMemo } from "react";
-import CSS, { Property } from "csstype";
+import Image from "next/image";
 
 type PropertyCardType = {
-  imageDimensions?: string;
-  imageDimensionIds?: string;
-  imageDimensionValues?: string;
-  imageDimensionIdsAndValue?: string;
-  imageDimensionIdsAndValue2?: string;
-  imageDimensionIdsAndValue3?: string;
-  imageDimensionIdsAndValue4?: string;
-  imageDimensionIdsAndValue5?: string;
-
-  /** Style props */
-  propWidth?: Property.Width;
-  propFlex?: Property.Flex;
-  propMinWidth?: Property.MinWidth;
-  propMaxWidth?: Property.MaxWidth;
+  imgUrl?: string;
+  name?: string;
+  price?: number;
 };
 
-const PropertyCard: NextPage<PropertyCardType> = ({
-  imageDimensions,
-  imageDimensionIds,
-  imageDimensionValues,
-  imageDimensionIdsAndValue,
-  imageDimensionIdsAndValue2,
-  imageDimensionIdsAndValue3,
-  imageDimensionIdsAndValue4,
-  imageDimensionIdsAndValue5,
-  propWidth,
-  propFlex,
-  propMinWidth,
-  propMaxWidth,
-}) => {
-  const card1Style: CSS.Properties = useMemo(() => {
-    return {
-      width: propWidth,
-      flex: propFlex,
-      minWidth: propMinWidth,
-      maxWidth: propMaxWidth,
-    };
-  }, [propWidth, propFlex, propMinWidth, propMaxWidth]);
-
+const PropertyCard: NextPage<PropertyCardType> = ({ imgUrl, name, price }) => {
   return (
-    <div
-      className="rounded-3xs bg-gray-white box-border w-[350px] h-[466.99px] flex flex-col py-[15px] px-[13px] items-start justify-start gap-[23px] text-left text-[14.51px] text-gray-700 font-body-regular-600 border-[1px] border-solid border-whitesmoke-100 hover:bg-gainsboro hover:cursor-pointer sm:w-[100%!important] sm:mb-5"
-      style={card1Style}
-    >
-      <img
-        className="self-stretch relative rounded-3xs max-w-full overflow-hidden h-[200px] shrink-0 object-cover"
-        alt=""
-        src={imageDimensions}
+    <div className='rounded-3xs bg-gray-white box-border w-[350px] h-[466.99px] flex flex-col py-[15px] px-[13px] items-start justify-start gap-[23px] text-left text-[14.51px] text-gray-700 font-body-regular-600 border-[1px] border-solid border-whitesmoke-100 hover:bg-gainsboro hover:cursor-pointer sm:w-[100%!important] sm:mb-5'>
+      <Image
+        width={300}
+        height={300}
+        className=' rounded-3xs max-w-full overflow-hidden h-[200px] shrink-0 object-cover'
+        alt=''
+        src={imgUrl!}
       />
-      <div className="self-stretch flex flex-row p-2.5 items-start justify-start">
-        <div className="flex-1 relative leading-[20.32px] font-medium">
-          92 ALLIUM PLACE, ORLANDO FL 32827
+      <div className=' flex flex-row p-2.5 items-start justify-start'>
+        <div className='flex-1 relative leading-[20.32px] font-medium'>
+          {name}
         </div>
       </div>
-      <div className="self-stretch flex flex-row p-2.5 items-start justify-start text-[13.55px] text-primary-500">
-        <div className="relative leading-[18.98px] font-semibold">
-          $ 590,693
+      <div className='flex flex-row p-2.5 items-start justify-start text-[13.55px] text-primary-500'>
+        <div className='relative leading-[18.98px] font-semibold'>
+          $ {price}
         </div>
       </div>
-      <div className="self-stretch flex flex-row py-0 px-2.5 items-start justify-start gap-[17px] text-[10.84px] text-gray-500">
-        <div className="flex flex-row items-center justify-start gap-[4.35px]">
-          <img
-            className="relative w-[17.42px] h-[17.42px]"
-            alt=""
-            src={imageDimensionIds}
+      <div className='flex flex-row py-0 px-2.5 items-start justify-start gap-[17px] text-[10.84px] text-gray-500'>
+        <div className='flex flex-row items-center justify-start gap-[4.35px]'>
+          <Image
+            width={300}
+            height={300}
+            className='w-[17.42px] h-[17.42px]'
+            alt=''
+            src='/car.svg'
           />
-          <div className="relative leading-[16.27px] font-medium">4</div>
+          <div className='relative leading-[16.27px] font-medium'>4</div>
         </div>
-        <div className="flex flex-row items-center justify-start gap-[4.35px]">
-          <img
-            className="relative w-[17.42px] h-[17.42px]"
-            alt=""
-            src={imageDimensionValues}
+        <div className='flex flex-row items-center justify-start gap-[4.35px]'>
+          <Image
+            width={300}
+            height={300}
+            className='w-[17.42px] h-[17.42px]'
+            alt=''
+            src='/bathtub.svg'
           />
-          <div className="relative leading-[16.27px] font-medium">4</div>
+          <div className='relative leading-[16.27px] font-medium'>4</div>
         </div>
-        <div className="flex flex-row items-center justify-start gap-[4.35px]">
-          <img
-            className="relative w-[17.42px] h-[17.42px]"
-            alt=""
-            src={imageDimensionIdsAndValue}
+        <div className='flex flex-row items-center justify-start gap-[4.35px]'>
+          <Image
+            width={300}
+            height={300}
+            className='w-[17.42px] h-[17.42px]'
+            alt=''
+            src='/arrowsout.svg'
           />
-          <div className="relative leading-[16.27px] font-medium">
+          <div className='relative leading-[16.27px] font-medium'>
             2,096.00 ft
           </div>
         </div>
       </div>
-      <div className="self-stretch flex flex-row items-center justify-between text-center text-[11.61px]">
-        <div className="flex flex-row items-center justify-start gap-[5.81px]">
-          <img
-            className="relative rounded-[50%] w-[27.57px] h-[27.57px] object-cover"
-            alt=""
-            src={imageDimensionIdsAndValue2}
+      <div className='w-full flex flex-row items-center justify-between text-center text-[11.61px]'>
+        <div className='flex flex-row items-center justify-start gap-[5.81px]'>
+          <Image
+            width={300}
+            height={300}
+            className='rounded-[50%] w-[27.57px] h-[27.57px] object-cover'
+            alt=''
+            src='/ellipse-1@2x.png'
           />
-          <div className="relative leading-[17.42px] font-medium">
+          <div className='relative leading-[17.42px] font-medium'>
             Jenny Wilson
           </div>
         </div>
-        <div className="flex flex-row items-start justify-start gap-[8.71px]">
-          <div className="rounded-[1.45px] bg-primary-50 flex flex-row p-[2.902620315551758px] items-start justify-start">
-            <img
-              className="relative w-[14.51px] h-[14.51px]"
-              alt=""
-              src={imageDimensionIdsAndValue3}
+        <div className='flex flex-row items-start justify-start gap-[8.71px]'>
+          <div className='rounded-[1.45px] bg-primary-50 flex flex-row p-[2.902620315551758px] items-start justify-start'>
+            <Image
+              width={300}
+              height={300}
+              className='w-[14.51px] h-[14.51px]'
+              alt=''
+              src='/sharenetwork.svg'
             />
           </div>
-          <div className="rounded-[1.45px] bg-primary-50 flex flex-row p-[2.902620315551758px] items-start justify-start">
-            <img
-              className="relative w-[14.51px] h-[14.51px]"
-              alt=""
-              src={imageDimensionIdsAndValue4}
+          <div className='rounded-[1.45px] bg-primary-50 flex flex-row p-[2.902620315551758px] items-start justify-start'>
+            <Image
+              width={300}
+              height={300}
+              className='w-[14.51px] h-[14.51px]'
+              alt=''
+              src='/heart.svg'
             />
           </div>
-          <div className="rounded-[1.45px] bg-primary-50 flex flex-row p-[2.902620315551758px] items-start justify-start">
-            <img
-              className="relative w-[14.51px] h-[14.51px]"
-              alt=""
-              src={imageDimensionIdsAndValue5}
+          <div className='rounded-[1.45px] bg-primary-50 flex flex-row p-[2.902620315551758px] items-start justify-start'>
+            <Image
+              width={300}
+              height={300}
+              className='w-[14.51px] h-[14.51px]'
+              alt=''
+              src='/plus.svg'
             />
           </div>
         </div>
